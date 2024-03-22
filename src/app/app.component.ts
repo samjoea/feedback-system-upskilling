540e1fb134d550c6f3414c82bd159da5710fdb96
Christian Jones <ekumkofi@gmail.com>
2024-03-22 00:54:37 +0000

    christian first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'feedback-system-upskilling';
+  title = 'feedback-system-upskilling - christian jones';
 }

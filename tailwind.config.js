e9debd2702c9cc893d18c4f919a43d73cd35ffda
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-05 13:50:31 +0000

    feat: created a color template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  theme: {},
+  theme: {
+    colors: {
+      'purple': '#AD1FEA',
+      'blue-deep': '#4661E6',
+      'black-deep': 'rgb(55, 63, 104)',
+      'white': '#FFFFFF',
+      'black-lighter': '#F2F4FF',
+      'gray': '#647196',
+      'gray-light': '#F7F8FD',
+      'black': '#3A4374',
+      'gray-deep': '#647196',
+      'orange': '#F49F85',
+      'blue-light': '#62BCFA',
+      'purple-hover': '#C75AF6',
+      'blue-hover': '#CFD7FF',
+    }
+  },
   plugins: [],
 };
